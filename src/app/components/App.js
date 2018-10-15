f2b0976f3a78fdeb5ac89fabf9ffa364c81094f0
fonbah <fonbah9@gmail.com>
2018-10-15 18:27:37 -0400

    commented code removed

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -2,24 +2,8 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
-//import { sendTodos, loadTodos } from '../actions/todos'
-//import { ipfs, room } from '../ipfs'
 
-import IPFS from 'ipfs'
-import Room from 'ipfs-pubsub-room'
-
-const App = ({ dispatch }) => {
-    // ipfs.once('ready', () => ipfs.id((err, info) => {
-    //     room.on('peer joined', (peer) => {
-    //         console.log('peer ' + peer + ' joined')
-    //         dispatch(sendTodos(peer))
-    //     })
-    //     room.on('message', (message) => {
-    //         console.log('got message from ' + message.from + ': ' + message.data.toString())
-    //         dispatch(loadTodos(JSON.parse(message.data.toString())))
-    //     })
-        
-    // }))
+const App = () => {
     return <div className="container-fluid">
         <AddTodo />
         <VisibleTodoList />
--- src/app/main.js
+++ src/app/main.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+//import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
 import { ipfs, room } from './ipfs'
